29a41020f092bc7789fb6f5c65e286d25c6f513b
wushuyi <wushuyi541@gmail.com>
2015-09-30 23:35:09 +0800

    Processing progress

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ var gulp = require('gulp');
 var run = require('gulp-run');
 
 gulp.task('default', ['build-css', 'build-js'], function () {
-    run('echo "ok!').exec();
+    run('echo "build...!"').exec();
 });
 
 gulp.task('build-css', function () {
-    run("lessc --no-color --autoprefix assets/css/style.less > assets/css/style.css").exec();
+    run("npm run build-css").exec();
 });
 
 gulp.task('build-js', ['clear-cache'], function () {
-    run('jspm bundle ./assets/js/main ./build/build.js --minify --inject').exec();
+    run('npm run build-js').exec();
 });
 
 gulp.task('build-cache', ['clear-cache'], function () {
-    run('jspm bundle ./bundle.config.js ./cache/cache.js --inject').exec();
+    run('npm run build-cache').exec();
 });
 gulp.task('clear-cache', function () {
-    run('jspm unbundle').exec();
+    run('npm run clear-cache').exec();
 });
